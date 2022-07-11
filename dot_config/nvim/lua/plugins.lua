347c3d3a2d6ee99f724a4fe5dca32877dfaa035b
TreverHibbs <treverhibbs@gmail.com>
2022-07-10 18:50:35 -0700

    setup lsp

---
+++ dot_config/nvim/lua/plugins.lua
@@ -0,0 +1,23 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+-- vim.cmd [[packadd packer.nvim]]
+
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+  augroup end
+]])
+
+return require('packer').startup(function()
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+  use {
+    'junnplus/nvim-lsp-setup',
+    requires = {
+        'neovim/nvim-lspconfig',
+        'williamboman/nvim-lsp-installer',
+    }
+  }
+end)
