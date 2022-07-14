5e2b743d5c3d91830106277abdb3c4c46f49968e
TreverHibbs <treverhibbs@gmail.com>
2022-07-14 11:42:23 -0700

    add auto launch to default session

--- dot_config/windows-term-sessions/empty_default.sh
+++ dot_config/windows-term-sessions/empty_default.sh
@@ -1,7 +1,7 @@
 project_dir="$HOME/.local/share/chezmoi"
 
 cmd.exe /c "wt.exe" \
-    --title editor --startingDirectory $project_dir \; \
+    --title editor --startingDirectory $project_dir --profile "manjaro start nvim" \; \
     split-pane --horizontal --size .25 --startingDirectory $project_dir \; \
     new-tab --title server --startingDirectory $project_dir \; \
     focus-tab -t 0
