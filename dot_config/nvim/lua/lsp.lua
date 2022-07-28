25ea9e3ebf6d134a899ec88b05cdd47273aa04cb
TreverHibbs <treverhibbs@gmail.com>
2022-07-28 10:28:07 -0700

    add nvim aliases

--- dot_config/nvim/lua/lsp.lua
+++ dot_config/nvim/lua/lsp.lua
@@ -15,42 +15,25 @@ require("mason").setup({
     }
 })
 
-require("mason-lspconfig").setup({
-    ensure_installed = { "sumneko_lua" },
-})
+require("mason-lspconfig").setup()
+require("mason-lspconfig").setup_handlers {
+    -- The first entry (without a key) will be the default handler
+    -- and will be called for each installed server that doesn't have
+    -- a dedicated handler.
+    function(server_name) -- default handler (optional)
+        require("lspconfig")[server_name].setup(coq.lsp_ensure_capabilities({
+            on_attach = Keybinds.on_attach,
+        }))
+    end
+    -- Next, you can provide targeted overrides for specific servers.
+    -- For example, a handler override for the `rust_analyzer`:
+    -- ["rust_analyzer"] = function()
+    --     require("rust-tools").setup {}
+    -- end
+}
 
-require 'lspconfig'.sumneko_lua.setup(coq.lsp_ensure_capabilities({
-    on_attach = Keybinds.on_attach,
-}))
-require 'lspconfig'.html.setup(coq.lsp_ensure_capabilities({
-    on_attach = Keybinds.on_attach,
-}))
-
--- require('nvim-lsp-setup').setup(coq.lsp_ensure_capabilities({
---     servers = {
---         sumneko_lua = {
---             Lua = {
---                 diagnostics = {
---                     -- Get the language server to recognize the `vim` global
---                     globals = { 'vim' },
---                 },
---                 runtime = {
---                     ['special'] = {
---                         ["include"] = "require"
---                     },
---                     path = runtime_path,
---                 },
---                 workspace = {
---                     -- Make the server aware of Neovim runtime files
---                     library = {
---                         vim.fn.expand('$VIMRUNTIME/lua'),
---                         vim.fn.stdpath('config') .. '/lua'
---                     }
---                 },
---             }
---         },
---         html = {},
---         tsserver = {},
---         jsonls = {}
---     }
--- }))
+--require 'lspconfig'.sumneko_lua.setup(coq.lsp_ensure_capabilities({
+--}))
+--require 'lspconfig'.html.setup(coq.lsp_ensure_capabilities({
+--    on_attach = Keybinds.on_attach,
+--}))
